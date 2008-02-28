da48bc353374e1eecf0aa7069b596d97eecde24d
Rob Sanheim <rsanheim@gmail.com>
2008-02-28 14:32:35 -0500

    better install

--- install.rb
+++ install.rb
@@ -1,6 +1,7 @@
 puts
-print " !brain_buster successfully installed!"
-print " Please see the README for detailed instructions, or join the google group"
-print " at http://groups.google.com/group/brainbuster-discuss if you need more help."
-print " Thanks for playing with the brain buster."
+puts " ****** BrainBuster ******* successfully installed!"
+puts " Please see the README for detailed instructions, or join the google group"
+puts " at http://groups.google.com/group/brainbuster-discuss if you need more help."
+puts " Please log bugs at http://opensource.thinkrelevance.com/"
+puts " Thanks for playing with the BrainBuster."
 puts
