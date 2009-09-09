baaf7abde5307664eb51ab6182e5931326a2258b
Mereghost <marcello.rocha@gmail.com>
2009-09-10 06:41:00 +0800

    Requiring delegate for ruby 1.9 compatiblity
    Signed-off-by: Mereghost <marcello.rocha@gmail.com>
    
    Signed-off-by: Rob Sanheim <rsanheim@gmail.com>

--- lib/humane_integer.rb
+++ lib/humane_integer.rb
@@ -1,6 +1,8 @@
 # Delegate to Integer to allow translating numbers to English words
 # Credit goes to  Ruby Quiz #25 http://www.rubyquiz.com/quiz25.html
 # and Glenn Parker's solution http://blade.nagaokaut.ac.jp/cgi-bin/scat.rb/ruby/ruby-talk/135449
+require 'delegate'
+
 class HumaneInteger < DelegateClass(Integer)
   Ones = %w[ zero one two three four five six seven eight nine ]
   Teen = %w[ ten eleven twelve thirteen fourteen fifteen
